e050ea269f96642515eca3eda8b472c9cd385840
aborashed94 <m.al.omar94@gmail.com>
2022-03-19 17:52:18 +0400

    add scss

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view/>
+    <v-main class="main-view">
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 };
 </script>
+<style lang="scss">
+.main-view {
+  background-color: $dimgray;
+}
+</style>
